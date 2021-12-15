e0886baf1e0e99067bfe6c984b5306b7e1bd259d
Christopher Green <chrisgwgreen@gmail.com>
2021-12-15 17:38:31 +0000

    feat: new eyes

---
+++ src/components/Eyes.tsx
@@ -0,0 +1,111 @@
+import { useRef, useEffect } from 'react'
+import * as THREE from 'three'
+import styled from 'styled-components'
+import 'assets/index.css'
+import { getTexture } from 'assets/texture'
+
+const EyeWrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+`
+
+export const Eyes = () => {
+  const mount = useRef<HTMLDivElement>(document.createElement('div'))
+
+  useEffect(() => {
+    const mountCurrent = mount.current
+    let width = mountCurrent.clientWidth
+    let height = mountCurrent.clientHeight
+
+    const scene = new THREE.Scene()
+    const camera = new THREE.OrthographicCamera(
+      width / -64,
+      width / 64,
+      height / 64,
+      height / -64,
+      1,
+      10000
+    )
+    const renderer = new THREE.WebGLRenderer({ antialias: true })
+    const geometry = new THREE.SphereGeometry(1, 64, 64)
+
+    const materials = [
+      new THREE.MeshBasicMaterial({ map: getTexture(0) }),
+      new THREE.MeshBasicMaterial({ map: getTexture(1) })
+    ]
+
+    const eyes: THREE.Mesh[] = []
+
+    camera.position.z = 30
+
+    renderer.setClearColor('#eee')
+    renderer.setSize(width, height)
+    renderer.setPixelRatio(window.devicePixelRatio)
+
+    const wn = Math.ceil(width / 90 / 2)
+    const hn = Math.ceil(height / 90 / 2)
+    let u = 0
+
+    for (let x = -wn; x < wn; x++) {
+      for (let y = -hn; y < hn; y++) {
+        u++
+
+        eyes[x + y] = new THREE.Mesh(
+          geometry,
+          materials[Math.floor(Math.random() * materials.length)]
+        )
+        eyes[x + y].position.x = x * 2.5
+        eyes[x + y].position.y = y * 2.5
+        eyes[x + y].rotation.x += -1.5
+
+        scene.add(eyes[x + y])
+      }
+    }
+
+    console.log(u)
+
+    const renderScene = () => {
+      renderer.render(scene, camera)
+    }
+
+    const handleResize = () => {
+      width = mountCurrent.clientWidth
+      height = mountCurrent.clientHeight
+      renderer.setSize(width, height)
+      camera.updateProjectionMatrix()
+      renderScene()
+    }
+
+    const animate = () => {
+      eyes[0].rotation.x += 0.01
+      // eyes[1].rotation.x += 0.01
+
+      // eyes[0].rotation.z += 0.01
+      // eyes[1].rotation.z += 0.01
+
+      renderScene()
+      requestAnimationFrame(animate)
+    }
+
+    requestAnimationFrame(animate)
+
+    mountCurrent.appendChild(renderer.domElement)
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+      mountCurrent.removeChild(renderer.domElement)
+      geometry.dispose()
+
+      for (let i = 0; i < eyes.length; i++) {
+        scene.remove(eyes[i])
+      }
+
+      for (let i = 0; i < materials.length; i++) {
+        materials[i].dispose()
+      }
+    }
+  }, [])
+
+  return <EyeWrapper ref={mount} />
+}
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export interface rgb {
+  r: number
+  g: number
+  b: number
+}
